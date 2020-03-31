c20c06cbfe15e3a09e911929cd7882235e242d83
RobnettSean22 <robnettsean22@gmail.com>
2020-04-01 03:02:58 +0900

    sized layout out. added background colors to div

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      search: ""
+    };
   }
 
   render() {
     return (
       <div id='archive-container'>
         <div id='search'>
-          <input />
+          <input onChange={e => this.setState({ search: e.target.value })} />
         </div>
         <div id='title-count'>
-          <h3></h3>
+          <h3>Results: 0 mail(s)</h3>
         </div>
         <div id='email'>
           <div id='mail-order'>
@@ -26,14 +28,7 @@ class App extends Component {
               <li>Date</li>
             </ul>
           </div>
-          <div id='mail-content'>
-            <ul>
-              <li>From</li>
-              <li>To</li>
-              <li>Subject</li>
-              <li>Date</li>
-            </ul>
-          </div>
+          <div id='mail-content'>map data goes here</div>
         </div>
       </div>
     );
