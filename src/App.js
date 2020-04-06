d87fe73e9e5fd07684a7a54da1e748e504382b70
RobnettSean22 <robnettsean22@gmail.com>
2020-04-07 05:01:14 +0900

    finish desktop and amobile queries. add a couple other queries for ipad

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import data from "./data/email-archives.json";
 import Clip from "./Assets/icon_clip.svg";
 import dateRange from "./Assets/icon_calender.svg";
 import MagGlass from "./Assets/icon_search.svg";
+import Up from "./Assets/icon_arrow01.svg";
+import Right from "./Assets/icon_arrow02.svg";
 
 class App extends Component {
   constructor(props) {
@@ -55,7 +57,7 @@ class App extends Component {
 
       .map((emails, i) => {
         return (
-          <div className='mail-list mobile-mail-list' key={i}>
+          <div className='mail-list ' key={i}>
             <div className='sender'>
               <h3>{emails.sender}</h3>
             </div>
@@ -79,29 +81,38 @@ class App extends Component {
               <h3>{emails.date}</h3>
             </div>
             <div className='mobile-sender'>
-              <div className='sender'>
+              <div className='mobilesender'>
                 <h3>{emails.sender}</h3>
+                <div className='mobile-date'>
+                  <div
+                    className={
+                      emails.attachment === true ? "mobile-attach" : "hide"
+                    }
+                  >
+                    <img src={Clip} alt='' />
+                  </div>
+                  <div
+                    className={
+                      emails.attachment === true
+                        ? "mobiledate"
+                        : "mobile-date-with-attachment"
+                    }
+                  >
+                    {" "}
+                    <h3>{emails.date}</h3>
+                    <div>
+                      <img src={Right} alt='' />
+                    </div>
+                  </div>
+                </div>
               </div>
-              <div className='recipient'>
+              <div className='mobile-recipient'>
                 {" "}
                 <h3>{emails.recipient}</h3>
               </div>
-              <div className='subject'>
+              <div className='mobile-subject'>
                 {" "}
                 <h3>{emails.subject}</h3>
-              </div>
-            </div>
-            <div className='mobile-date'>
-              <div className={emails.attachment === true ? "attach" : "hide"}>
-                <img src={Clip} alt='' />
-              </div>
-              <div
-                className={
-                  emails.attachment === true ? "date" : "date-with-attachment"
-                }
-              >
-                {" "}
-                <h3>{emails.date}</h3>
               </div>
             </div>
           </div>
@@ -153,17 +164,29 @@ class App extends Component {
           <div id='mail-order'>
             <div id='from'>
               <h2>From</h2>
+              <div>
+                <img src={Up} alt='' />
+              </div>
             </div>
             <div id='to'>
               <h2>To</h2>
+              <div>
+                <img src={Up} alt='' />
+              </div>
             </div>
             <div id='about'>
               {" "}
               <h2>Subject</h2>
+              <div>
+                <img src={Up} alt='' />
+              </div>
             </div>
             <div id='when'>
               {" "}
               <h2>Date</h2>
+              <div>
+                <img src={Up} alt='' />
+              </div>
             </div>
           </div>
           <div id='mail-content'>{filterEmails}</div>
