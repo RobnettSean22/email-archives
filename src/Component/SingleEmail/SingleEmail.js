5c3f13342ea071b072a884d492ccfedf9cf720cf
RobnettSean22 <robnettsean22@gmail.com>
2020-06-26 03:15:21 +0900

    css for singleemails

--- src/Component/SingleEmail/SingleEmail.js
+++ src/Component/SingleEmail/SingleEmail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SingleEmail.scss";
 
 const SingleEmail = props => {
   let toggleIndex = props.specIndex;
@@ -6,18 +7,35 @@ const SingleEmail = props => {
   console.log(props.mail);
   return (
     <div id='spec-email'>
-      <button onClick={e => props.forward()}></button>
-      <button onClick={e => props.back()}></button>
-      <button
-        onClick={e =>
-          props.emailWindow(
-            props.mail[toggleIndex].recipient,
-            props.mail[toggleIndex].subject,
-            props.mail[toggleIndex].info
-          )
-        }
-      ></button>
-      {props.mail[toggleIndex].info}
+      <div id='controls'>
+        <div id='toggle-controls'>
+          <button onClick={e => props.forward()}></button>
+          <button onClick={e => props.back()}></button>
+        </div>
+        <div id='new-window'>
+          <button
+            onClick={e =>
+              props.emailWindow(
+                props.mail[toggleIndex].recipient,
+                props.mail[toggleIndex].subject,
+                props.mail[toggleIndex].info
+              )
+            }
+          ></button>
+        </div>
+      </div>
+      <div id='contact'>
+        <h2>
+          {props.mail[toggleIndex].sender} to{" "}
+          {props.mail[toggleIndex].recipient}
+        </h2>
+      </div>
+      <div id='about'>
+        <h3>{props.mail[toggleIndex].subject}</h3>
+      </div>
+      <div id='content'>
+        <p>{props.mail[toggleIndex].info}</p>
+      </div>
     </div>
   );
 };
